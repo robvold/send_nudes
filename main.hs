bdbc27e19d5b2b359b403b93cd1ad0e9969e073f
robvold <robvold98@gmail.com>
2022-12-13 11:56:38 +0100

    executes the shell file

--- main.hs
+++ main.hs
@@ -1,19 +1,27 @@
 import Data.Time.Clock
 import Data.Time.Calendar
+import System.Process
 
 
 -- gets the current date
-date :: IO (Integer,Int,Int) -- :: (year,month,day)
-date = getCurrentTime >>= return . toGregorian . utctDay
+currentDate :: IO (Integer,Int,Int) -- :: (year,month,day)
+currentDate = getCurrentTime >>= return . toGregorian . utctDay
+
 
 -- creates shell file used to execute commits
-createShell :: IO ()
-createShell = do    
-    writeFile "example.sh" "echo \"hehe\""
+createShell :: String -> IO ()
+createShell fileName = do    
+    writeFile fileName "echo \"hehe\""
 
+-- executes the created shell file
+executeShell :: String -> IO ()
+executeShell str = do 
+    x <- readProcessWithExitCode "sh" [str] ""
+    print x
 
 main :: IO ()
-main = do
-    x <- date
-    print x
-    createShell
+main = let fileName = "example.sh" in do
+    date <- currentDate
+    print date
+    createShell fileName 
+    executeShell fileName
